b317f65c60681f00f79aaeb1831b0c81771f5835
riadhamami <riadh.amami@esprit.tn>
2021-07-21 18:29:11 +0100

    fixing shit

--- script.groovy
+++ script.groovy
@@ -8,7 +8,7 @@ def buildImage() {
     withCredentials([usernamePassword(credentialsId: 'docker-cred', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         sh 'docker build -t docker.galactechstudio.com/demo-app:jma-2.0 .'
         sh "echo $PASS | docker login https://docker.galactechstudio.com -u $USER --password-stdin"
-        sh 'docker push nexus.galactechstudio.com/demo-app:jma-2.0'
+        sh 'docker push docker.galactechstudio.com/demo-app:jma-2.0'
     }
 } 
 
